7d7535983c83bfd8445bdcd07eb46864272a91e4
Stopqur <dimyachmencev@gamil.ru>
2022-01-11 13:06:04 +0300

    edit route

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express =  require('express')
 const cors = require('cors')
 
 const db = require('./models')
-
+const authRouter = require('./routes/auth.routes')
 const app = express()
-
 const corsSetting = {
     origin: "http://localhost:5000"
   };
 
 app.use(cors(corsSetting));
 app.use(express.json())
+app.use('', (req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
-
-require('./routes/auth.routes')(app);
+app.use('/auth', authRouter)
+// require('./routes/auth.routes')(app);
 
 db.sequelize.sync();
 app.listen(5000, () => { console.log('working')})
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,19 @@
+const express = require('express')
+
 const { verifyReg } = require("../Middlewares");
 const controller = require("../controllers/auth.controllers");
 const validation = require('../Middlewares/validation')
 const userSchema = require('../Validations/userValidation')
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+const router = express.Router()
 
-  app.post(
-    "/auth/signup",
-    validation(userSchema),
-    [verifyReg.checkDuplicate],
-    controller.createUser
-  );
+router.post(
+  "/signup",
+  validation(userSchema),
+  [verifyReg.checkDuplicate],
+  controller.createUser
+);
 
-  app.post("/auth/signin", controller.authUser);
-};
+router.post("/signin", controller.authUser);
+
+module.exports = router
